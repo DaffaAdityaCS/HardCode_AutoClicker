5c1634a94b2c0e8f7347430208e2f3e8d7e56fa8
Daffa Aditya Rahman <41433315+DaffaAdityaCS@users.noreply.github.com>
2020-11-06 14:45:39 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,29 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+#tile 1: X: 598 Y:  711 RGB: (253,  18,   1)
+#tile 2: X: 687 Y:  719 RGB: (160, 165, 232)
+#tile 3: X:  792 Y:  718 RGB: (180, 183, 235)
+#tile 4: X:  884 Y:  716 RGB: (168, 172, 232)
+
+#membuat Def klik
+def click(x,y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+while keyboard.is_pressed("q") == False:
+
+    if pyautogui.pixel(587, 673)[0] == 0:
+        click(587, 673)
+    elif pyautogui.pixel(672, 676)[0] == 0:
+        click(672, 676)
+    elif pyautogui.pixel(776, 678)[0] == 0:
+        click(776, 678)
+    elif pyautogui.pixel(869, 673)[0] == 0:
+        click(869, 673)
---
+++ region_save.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+iml = pyautogui.screenshot(region=(640,350,630,450))
+iml.save(r"D:\programing\Project\python bot\saveimage.png")
