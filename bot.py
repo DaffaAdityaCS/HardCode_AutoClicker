f101a86297281c076c3a7b5cca88f0eab8410d7e
Daffa Aditya Rahman <41433315+DaffaAdityaCS@users.noreply.github.com>
2020-11-06 14:56:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,15 @@ import win32api, win32con
 #tile 3: X:  792 Y:  718 RGB: (180, 183, 235)
 #tile 4: X:  884 Y:  716 RGB: (168, 172, 232)
 
-#membuat Def klik
+#Make def mouse click
 def click(x,y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
+#emergency stop system
 while keyboard.is_pressed("q") == False:
-
+    #look every individual tale on the screen
     if pyautogui.pixel(587, 673)[0] == 0:
         click(587, 673)
     elif pyautogui.pixel(672, 676)[0] == 0:
