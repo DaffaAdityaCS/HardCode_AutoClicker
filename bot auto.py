c1368bdf48bcb2cc465b5cd823af430c258ca172
Daffa Aditya Rahman <41433315+DaffaAdityaCS@users.noreply.github.com>
2020-11-06 14:53:11 +0700

    Update bot auto.py

--- bot auto.py
+++ bot auto.py
@@ -5,6 +5,7 @@ import keyboard
 import random
 import win32api, win32con
 
+#looking for a picure like in pk.png and print if the picure same in the pk.png
 while 1:
     if pyautogui.locateOnScreen('pk.png' ,region=(0,150,400,350), grayscale=True, confidence=0.7) != None:
         print ("pk")
