cf65d367555e7cf6626f49fac6ca69103ef82255
Daffa Aditya Rahman <41433315+DaffaAdityaCS@users.noreply.github.com>
2020-11-06 14:58:38 +0700

    Update region_save.py

--- region_save.py
+++ region_save.py
@@ -1,4 +1,5 @@
 import pyautogui
-
+#taking screenshot on the region of choice
 iml = pyautogui.screenshot(region=(640,350,630,450))
+#save it on the directory
 iml.save(r"D:\programing\Project\python bot\saveimage.png")
